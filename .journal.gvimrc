9816be297210949e3948fe9cd4bdabbd16313fba
Garrett Everding <garrett.everding92@gmail.com>
2012-11-23 20:50:52 -0500

    Fixed height of gvim window that opens

--- .journal.gvimrc
+++ .journal.gvimrc
@@ -1,3 +1,4 @@
 
 set spell
 autocmd BufEnter * silent! lcd %:p:h
+set lines=50 columns=80 
--- .journal.sh
+++ .journal.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-/usr/bin/gvim -U ~/.journal.gvimrc -geometry 100x40+512+400 -c "call vimwiki#diary#make_note(v:count1)" + -c "r! date +'\%n= \%r =\%n'"
+/usr/bin/gvim -U ~/.journal.gvimrc -c "call vimwiki#diary#make_note(v:count1)" + -c "r! date +'\%n= \%r =\%n'"
 
