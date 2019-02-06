350db7669a73a999de9c6b2846fa746d45019f2f
Garrett Everding <garrett.everding92@gmail.com>
2019-02-06 14:06:33 -0500

    update

---
+++ lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[refactor-nrepl "2.4.0"]
+                  [nrepl/lein-nrepl "0.3.2"]
+                  [lein-kibit "0.1.6"]
+                  [lein-ancient "0.6.15"]
+                  [cider/cider-nrepl "0.19.0"]]}}
