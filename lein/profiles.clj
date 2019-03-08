3ce683775581278ee26bbbe8a4d8501ade8431e9
Garrett Everding <garrett.everding92@gmail.com>
2019-03-07 23:18:15 -0500

    update lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,20 @@
-{:user {:plugins [[refactor-nrepl "2.4.0"]
-                  [nrepl/lein-nrepl "0.3.2"]
-                  [lein-kibit "0.1.6"]
-                  [lein-ancient "0.6.15"]
-                  [cider/cider-nrepl "0.19.0"]]}}
+{:repl
+ {:plugins [[cider/cider-nrepl "0.21.2-SNAPSHOT"]
+            [com.billpiel/sayid "0.0.17"]
+            [refactor-nrepl "2.4.0"]
+            ]
+  :dependencies [[nrepl "0.6.0"]]
+  :repl-options
+  {:nrepl-middleware [cider.nrepl/cider-middleware
+                      refactor-nrepl.middleware/wrap-refactor ;; clj-refactor
+                      com.billpiel.sayid.nrepl-middleware/wrap-sayid ;; sayid
+                      ]}
+  }
+ :user
+ {:plugins  [[refactor-nrepl "2.4.0"]
+             [cider/cider-nrepl "0.21.2-SNAPSHOT"]
+             [nrepl/lein-nrepl "0.3.2"]
+             [com.billpiel/sayid "0.0.17"]
+             [lein-kibit "0.1.6"]
+             [lein-cljfmt "0.6.4"]
+             [lein-ancient "0.6.15"]]}}
