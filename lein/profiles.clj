e779c6eeb6e9e87ab0af04e48e5e40260031eec7
Garrett Everding <garrett.everding92@gmail.com>
2019-12-19 12:55:16 -0500

    update lien

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,18 +1,18 @@
 {:repl
  {:plugins [[cider/cider-nrepl "0.22.3"]
             [com.billpiel/sayid "0.0.17"]
-            [refactor-nrepl "2.5.0-SNAPSHOT"]
+            ;; [refactor-nrepl "2.5.0-SNAPSHOT"]
             ]
   :dependencies [[nrepl "0.6.0"]]
-  :repl-options
-  {:nrepl-middleware [cider.nrepl/cider-middleware
-                      refactor-nrepl.middleware/wrap-refactor ;; clj-refactor
-              ;;        com.billpiel.sayid.nrepl-middleware/wrap-sayid ;; sayid
-                      ]}
+  ;; :repl-options
+  ;; {:nrepl-middleware [cider.nrepl/cider-middleware
+  ;;                     refactor-nrepl.middleware/wrap-refactor ;; clj-refactor
+  ;;             ;;        com.billpiel.sayid.nrepl-middleware/wrap-sayid ;; sayid
+  ;;                     ]}
   }
  :user
  {;;:dependencies [[com.bhauman/rebel-readline "0.1.4" :exclusions [com.google.protobuf/protobuf-java]]]
- {:plugins  [[refactor-nrepl "2.5.0-SNAPSHOT"]
+ {:plugins  [#_[refactor-nrepl "2.5.0-SNAPSHOT"]
              [cider/cider-nrepl "0.22.3"]
              [nrepl/lein-nrepl "0.3.2"]
              ;;[com.billpiel/sayid "0.0.17"]
