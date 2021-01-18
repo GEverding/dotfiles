68c55038e26d41120a910f2688cec59b6722520d
Garrett Everding <garrett.everding92@gmail.com>
2021-01-18 11:11:23 -0500

    update spacemacs lein and zsh

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,21 +1,10 @@
-{:repl
- {:plugins [[cider/cider-nrepl "0.22.3"]
-            [com.billpiel/sayid "0.0.17"]
-            ;; [refactor-nrepl "2.5.0-SNAPSHOT"]
-            ]
-  :dependencies [[nrepl "0.6.0"]]
-  ;; :repl-options
-  ;; {:nrepl-middleware [cider.nrepl/cider-middleware
-  ;;                     refactor-nrepl.middleware/wrap-refactor ;; clj-refactor
-  ;;             ;;        com.billpiel.sayid.nrepl-middleware/wrap-sayid ;; sayid
-  ;;                     ]}
-  }
- :user
- {;;:dependencies [[com.bhauman/rebel-readline "0.1.4" :exclusions [com.google.protobuf/protobuf-java]]]
- {:plugins  [#_[refactor-nrepl "2.5.0-SNAPSHOT"]
-             [cider/cider-nrepl "0.22.3"]
+{:user {
+  :dependencies [ [ nrepl "0.8.3" ] ]
+  :plugins  [[refactor-nrepl "2.5.0"]
+             [cider/cider-nrepl "0.25.5"]
              [nrepl/lein-nrepl "0.3.2"]
              ;;[com.billpiel/sayid "0.0.17"]
+             [cider/piggieback "0.5.2"]
              [lein-kibit "0.1.6"]
              [lein-cljfmt "0.6.4"]
              [lein-ancient "0.6.15"]]}}
