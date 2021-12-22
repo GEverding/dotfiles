e66a9bbd5b5f5c226caaad1431ca1e42e8fd5e47
Garrett Everding <garrett.everding92@gmail.com>
2021-12-21 19:30:26 -0500

    updates

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {
   :dependencies [ [ nrepl "0.8.3" ] ]
-  :plugins  [[refactor-nrepl "2.5.0"]
-             [cider/cider-nrepl "0.25.5"]
+  :plugins  [[refactor-nrepl "3.1.0"]
+             [cider/cider-nrepl "0.27.2"]
              [nrepl/lein-nrepl "0.3.2"]
              ;;[com.billpiel/sayid "0.0.17"]
-             [cider/piggieback "0.5.2"]
-             [lein-kibit "0.1.6"]
-             [lein-cljfmt "0.6.4"]
+             #_[cider/piggieback "0.5.2"]
+             #_[lein-kibit "0.1.6"]
+             #_[lein-cljfmt "0.6.4"]
              [lein-ancient "0.6.15"]]}}
